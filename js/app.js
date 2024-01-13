fd7e4bfdbf95336374a315646c0399e32b967fc2
joninoni <jonibeltranhayer@gmail.com>
2024-01-13 16:05:04 -0600

    agregando la funcion de la alerta

--- js/app.js
+++ js/app.js
@@ -12,7 +12,24 @@ function validarFormulario(e){
     e.preventDefault();
     const terminoBusqueda=document.querySelector("#termino").value;
     if(terminoBusqueda===""){
-        console.log("no puede haber campos vacios");
+        mostrarAlerta("Agrega un termino de busqueda");
         return;
+    }
+}
+
+function mostrarAlerta(mensaje){
+    const existe=document.querySelector(".bg-red-100");
+    if (!existe) {
+        const alerta=document.createElement("p");
+        alerta.classList.add("bg-red-100","border-red-400","text-red-700","px-4","py-3","rounded","max-w-lg","mx-auto","mt-6","text-center");
+        alerta.innerHTML=
+        `
+            <strong class="font-bold">Error!</strong>
+            <span class="block sm:inline">${mensaje}</span>
+        `;
+        formulario.appendChild(alerta);
+        setTimeout(() => {
+            alerta.remove();
+        }, 2000);
     }
 }
